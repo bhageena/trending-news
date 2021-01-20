e9cc0d04b4bd2a71280dad5ab1428ca85fc4e151
Chandan Rai <dev.chandan.rai@gmail.com>
2021-01-20 21:24:56 +0530

    Initial commit

---
+++ test/trending-news/get-trending-topics.test.go
@@ -0,0 +1,27 @@
+package gettrendingtopics
+
+import (
+	"testing"
+    "../../packages/trending-news/get-trending-topics/index"
+)
+        
+func TestMain(t *testing.T) {
+    response := main()
+    if response != "" {
+       t.Errorf("Actual response body doesn't match expected response")
+    }
+
+    expected := ""
+    if response.Body.String() != expected {
+        t.Errorf("handler returned unexpected body: got %v want %v",
+            response.Body.String(), expected)
+    }
+}
+
+func TestOkRequest(t *testing.T) {
+    response := main()
+    if response.statusCode != 200  {
+       t.Errorf("Request was not successful")
+    }
+}
+
