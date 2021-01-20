5866f9129c077d0df1bfb64cc55939222957f267
Chandan Rai <dev.chandan.rai@gmail.com>
2021-01-20 21:24:54 +0530

    Initial commit

---
+++ test/trending-news/get-bearer-token.test.go
@@ -0,0 +1,27 @@
+package getbearertoken
+
+import (
+	"testing"
+    "../../packages/trending-news/get-bearer-token/index"
+)
+        
+func TestMain(t *testing.T) {
+    response := main()
+    if response != "" {
+       t.Errorf("Actual response body doesn't match expected response")
+    }
+
+    expected := ""
+    if response.Body.String() != expected {
+        t.Errorf("handler returned unexpected body: got %v want %v",
+            response.Body.String(), expected)
+    }
+}
+
+func TestPostRequest(t *testing.T) {
+    response := main()
+    if response.code != 201 || response.code != 202  {
+       t.Errorf("POST request was not successful")
+    }
+}
+
