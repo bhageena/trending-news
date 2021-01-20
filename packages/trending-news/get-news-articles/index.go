038071c0e44cd3e3729aa9abde7bf065a293f480
Chandan Rai <dev.chandan.rai@gmail.com>
2021-01-20 21:24:49 +0530

    Initial commit

---
+++ packages/trending-news/get-news-articles/index.go
@@ -0,0 +1,9 @@
+package main
+
+
+/*Main Requests news articles from newsapi.org with query text and language as English */
+func Main(args map[string]interface{}) map[string]interface{} {
+	msg := make(map[string]interface{})
+	msg["body"] = ""
+	return msg
+}
